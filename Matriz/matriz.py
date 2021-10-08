7091d8b55b1a83398b4446ccbb2d0225c3393a8b
Rocio <rocioreginaottavianelli@gmail.com>
2021-10-07 21:56:40 -0300

    ejercicio terminado

--- Matriz/matriz.py
+++ Matriz/matriz.py
@@ -9,18 +9,32 @@ def createMatrix(columns, rows):
     return matrix
 
 def findSequence(matrix, sequence):
-    for i in range(len(matrix)):
-        j = 0
-        while (j < len(matrix[i]) - sequence):
-            found = 1 #cantidad de numeros consecutivos encontrados en la iteracion
-            auxIndex = j #buscando verticalmente, el indice que aumenta sera el de filas
-            while (found < sequence) and (matrix[i][auxIndex] == matrix[i][auxIndex] - 1):
+    #esta solucion funciona solo cuando la cantidad de filas es la misma que de columnas
+    for x in range(len(matrix)):
+        y = 0
+        while (y < len(matrix[x]) - sequence):
+            foundVertical = 1 #cantidad de numeros consecutivos encontrados en la iteracion
+            foundHorizontal = 1
+            auxIndex = y
+
+            #buscando verticalmente
+            while (foundVertical < sequence) and (matrix[x][auxIndex] == matrix[x][auxIndex] - 1):
                 auxIndex += 1
-                found += 1
-            if(found == sequence): #si while corta por hallar 4 consecutivos
-                return {"initial": [i, j], "final": [i, auxIndex]}
-            j += 1
+                foundVertical += 1
+            if(foundVertical == sequence): #si while corta por hallar 4 consecutivos
+                return {"initial": [x, y], "final": [x, auxIndex]}
+
+            #buscando horizontalmente
+            while (foundHorizontal < sequence) and (matrix[auxIndex][x] == matrix[auxIndex][x] - 1):
+                auxIndex += 1
+                foundHorizontal += 1
+            if(foundVertical == sequence): #si while corta por hallar 4 consecutivos
+                return {"initial": [y, x], "final": [auxIndex, x]}
+            y += 1
     return -1
 
 newMatrix = createMatrix(columns, rows)
-positions = findConsecutives(newMatrix, sequenceLength)
+positions = findSequence(newMatrix, sequenceLength)
+if(positions != -1):
+    print("Posicion inicial y final de la secuencia: ", positions)
+else: print("No se encontro una secuencia de numeros consecutivos")
