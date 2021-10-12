d1a9ae4169ef6f747d83a1372b218c39f727a8be
Rocio <rocioreginaottavianelli@gmail.com>
2021-10-12 13:48:35 -0300

    avance en la solucion parcial

--- Clases/clases.py
+++ Clases/clases.py
@@ -1,29 +1,40 @@
 import math
 
-class ValueIsZero(Exception):
+class CustomError(Exception):
+    pass
 
-    def __init__(self, message="El radio debe ser mayor a cero."):
+class ValueIsZero(CustomError):
+    #excepcion custom
+    def __init__(self, message="Numero invalido"):
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
         return self.message
 
+def verifyValue(num):
+    #esta funcion verifica que el valor ingresado sea mayor a cero
+    try:
+        if(num <= 0):
+            raise ValueIsZero
+    except ValueIsZero:
+        print("Numero invalido : {}".format(num), ". Debe ser mayor a cero.")
+    else:
+        return num
+
 class Circulo:
 
     def __init__(self, radius):
         self.__radio = radius
 
-    def setRadius():
-        try:
-            radius = int(input("Ingrese un radio: "))
-            if (radius <= 0):
-                raise ValueIsZero
-            else:
-                self.__radio = radius
-            break
-        except ValueIsZero:
-            print("El radio debe ser mayor a cero. Ingrese otro valor: ")
+    def __str__(self):
+        #este string se retorna al imprimir el objeto
+        return "Radio: " + str(self.__radio) + ". Area: " + str(self.getArea()) + ". Perimetro: " + str(self.getPerimeter())
+
+    def setRadius(self):
+        radius = int(input("Ingrese un radio: "))
+        if(verifyValue(radius)):
+            self.__radio = radius
 
     def getArea(self):
         return math.pi * pow(self.__radio, 2)
@@ -31,15 +42,6 @@ class Circulo:
     def getPerimeter(self):
         return 2 * math.pi * self.__radio
 
-#prueba
-radius = int(input("Ingrese un radio para el nuevo circulo: "))
-try:
-    if(radius <= 0):
-        raise ValueIsZero
-    else:
-        newCircle = Circulo(radius)
-        print(newCircle.getArea())
-        print(newCircle.getPerimeter())
-    break
-except ValueIsZero:
-    print("El radio debe ser mayor a cero. Ingrese otro valor: ")
+    def multiply(self, n):
+        if(verifyValue(n)):
+            return Circulo(self.__radio * n)
